1988405d1970b38d31b73374962ce3bdae54fb75
GavrSU <127018780+GavrSU@users.noreply.github.com>
2024-01-07 22:02:54 +0700

    123

--- creatures/base_creature/base_creature.gd
+++ creatures/base_creature/base_creature.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
-var speed = 200
+class_name creature
+var speed = 500
+var attack_timer: float = 0
+var attack_cooldown: float = 1.0  # Время перезарядки атаки в секундах
+var attack_sprite = preload("res://bullet.png")
 func _ready():
 	pass
 func _physics_process(delta):
 	update_input_keyboard()
 	update_input_mouse()
 	move_and_slide()
+	attack_timer -= delta
 	
 func update_input_mouse():
 	# Получение позиции мыши в мировых координатах
@@ -13,6 +18,7 @@ func update_input_mouse():
 	# Вычисление вектора направления от персонажа к мыши
 	var direction: Vector2 = (mouse_position - global_position).normalized()
 	rotation = direction.angle()
+	attack()
 
 func update_input_keyboard():
 	velocity=Vector2()
@@ -27,4 +33,21 @@ func update_input_keyboard():
 
 	# Нормализация вектора скорости, чтобы движение по диагонали не было быстрее
 	velocity = velocity.normalized() * speed
+func attack():
+	if Input.is_action_pressed("attack") and attack_timer <= 0:
+		print("attack!")
+		var attack_instance = Sprite2D.new()
+		attack_instance.texture = attack_sprite  # Устанавливаем текстуру атаки
+		attack_instance.global_position = global_position  # Устанавливаем начальную позицию атаки
+		attack_instance.rotation = rotation  # Устанавливаем поворот атаки
+		add_child(attack_instance)  # Добавляем спрайт атаки к персонажу
+
+		# Запускаем таймер перезарядки атаки
+		attack_timer = attack_cooldown
+		await get_tree().create_timer(1).timeout
+		attack_instance.queue_free()
+		# Уничтожаем спрайт атаки через 1 секунду
+		print("пуля удалена")
+		
+		
 	
