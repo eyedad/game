6c8524acd76db22ccb64fb4d01f2606fb7ea7b0f
GavrSU <127018780+GavrSU@users.noreply.github.com>
2024-01-16 21:00:02 +0700

    pre alpha 0.05
    Корректные лучи

--- creatures/base_creature/fog_of_war.gd
+++ creatures/base_creature/fog_of_war.gd
@@ -1,31 +1,45 @@
 extends Node2D
 var ray: RayCast2D
-var child_ray: RayCast2D
+var child_ray: Array[RayCast2D]
+var enemy: Array[Creature]
 
-func fog_of_war(obj:Creature):
+func fog_of_war(obj:Creature):	
+	print(enemy, "\n")
 	if child_ray:
-		#print(child_ray.get_collider())
-		child_ray.queue_free()
-		#if child_ray.get_collider():
-			#if child_ray.get_collider().name == "skeleton":
-				#child_ray.get_collider().set_modulate(1, 1,  1, 1)
-		#
+		for child in child_ray:
+			if child != null:				
+				if child.get_collider() != null:
+					if child.get_collider().get_class() == "CharacterBody2D":
+						enemy.append(child.get_collider())
+						for element in enemy:
+							if element != null and obj.is_player:
+								element.visible = true
+			else:
+				for element in enemy:
+					if element != null and obj.is_player:
+						print(element)
+						element.visible = false
+				enemy = []
+
 		
-	if obj.enemy_in_detector_area:
+	for child in child_ray:
+		if child != null:
+			child.queue_free()	
+	for element in obj.enemy_in_detector_area:
+		
 		ray = RayCast2D.new()
-		
 		ray.set_hit_from_inside(true)
 		ray.collision_mask = 1
 		ray.collide_with_bodies = true
 		#ray.global_position = obj.position
-		ray.target_position = obj.enemy_in_detector_area[0].position - obj.position
+		ray.target_position = element.position - obj.position
 		obj.add_child(ray)
 		
 		
 		
-		for element in obj.get_children():
-			if element.get_class() == "RayCast2D":
-				child_ray = element
+		for child in obj.get_children():
+			if child.get_class() == "RayCast2D":
+				child_ray.append(child)
 				#print(child_ray)
 				
 				continue
